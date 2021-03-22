e3a80674e37fcb90cc52f650adc989bf468a1e77
Marco Monteiro <marco_monteiro@live.com>
2021-03-21 23:48:46 -0300

    Configurações e estilização

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,20 @@
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss';
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform',
+};
+
 export function RepositoryList() {
     return (
         <section className='repository-list'>
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href=''>Acessar repositório</a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href=''>Acessar repositório</a>
-                </li>
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     );
